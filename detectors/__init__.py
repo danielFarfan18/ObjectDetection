9b18547fcc6e0b514ca0bbcb33eafed2daf5a8cd
Daniel Farfán <119150840+danielFarfan18@users.noreply.github.com>
2025-05-14 21:21:50 -0600

    Create detectors folder

---
+++ detectors/__init__.py
@@ -0,0 +1,5 @@
+from .yolo4_detector import YOLO4Detector
+from .yolo5_detector import YOLO5Detector
+from .mobilenet_detector import MobileNetDetector
+
+__all__ = ['YOLO4Detector', 'YOLO5Detector', 'MobileNetDetector']
